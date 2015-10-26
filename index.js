3ce22a06479207d48496d8674f4904c323f199c6
weipeng <creatorkuang@gmail.com>
2015-10-26 16:19:30 +0800

    hide window in index.js

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ require('./style/index.less');
 
 // matchMedia polyfill for
 // https://github.com/WickyNilliams/enquire.js/issues/82
-window.matchMedia = window.matchMedia || function () {
-  return {
-    matches: false,
-    addListener: function () {
-    },
-    removeListener: function () {
-    }
-  };
-};
+// window.matchMedia = window.matchMedia || function () {
+//   return {
+//     matches: false,
+//     addListener: function () {
+//     },
+//     removeListener: function () {
+//     }
+//   };
+// };
 
 const antd = {
   Affix: require('./components/affix'),
